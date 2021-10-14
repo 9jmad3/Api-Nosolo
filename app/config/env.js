811c7c9d7b052935883fc032d0e0835ec455e4ae
9jmad3 <65808538+9jmad3@users.noreply.github.com>
2021-10-14 16:30:46 +0200

    heroku

--- app/config/env.js
+++ app/config/env.js
@@ -4,12 +4,7 @@
   password: 'ff70dccfc0b98c7c9fb410677176dde0866d520a442076ebd8c6c319a9e041e6',
   host: 'ec2-54-228-162-209.eu-west-1.compute.amazonaws.com',
   dialect: 'postgres',
-  pool: {
-	  max: 5,
-	  min: 0,
-	  acquire: 30000,
-	  idle: 10000
-  }
+  port: '5432'
 };
 module.exports = env;
 
